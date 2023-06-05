0d585ba9c7a2e41dc13c62341cc195f022085199
sfritzsche148 <96665383+sfritzsche148@users.noreply.github.com>
2023-06-05 19:35:56 +0200

    update main.js

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
-/*let createnewjournalbtn = document.getElementById('#createnewjournalbtn');
+let createnewjournalbtn = document.getElementById('#createnewjournalbtn');
 
 createnewjournalbtn.addEventlistener('click', function() {
     createnewjournalbtn.innerText = 'Hi';
-});*/
-
-if (1 === 1) {
-    window.alert('Hello')
-}
+});
